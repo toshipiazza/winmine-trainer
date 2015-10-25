c964558cceaaf0b0d3516522b3b8e5e1be26e69c
Toshi R Piazza <toshi.piazza@gmail.com>
2015-10-25 19:51:29 -0400

    Adds Readme/License, changes tabs to spaces

---
+++ README.md
@@ -0,0 +1,19 @@
+Winmine-Trainer
+===============
+
+Project 2 of Rensselaer Polytechnic Institute's Malware Analysis Course
+(CSCI 4976). Runs a trainer that allows you to talk to a winmine.exe instance
+and allows you to automatically win, show layout of tiles, etc.
+
+### How to Use
+Running trainer.exe opens up a console window and a winmine.exe instance.
+Running the commands should work as expected. Closing the console window will
+also kill the winmine.exe.
+
+### How to Build
+Notice! Only works on MSVC!
+
+```mkdir build
+cd build
+cmake ..
+msbuild ALL_BUILD.vxcproj```
--- trainer.cpp
+++ trainer.cpp
@@ -17,36 +17,38 @@ main(void)
 {
     std::cout << "Please wait... initializing winmine.exe" << std::endl;
 
-	unsigned int nOpt;
-	HANDLE hWinMine = CreateWinMineProcess();
-    HMODULE hAttackerModule;
+    unsigned int nOpt;
+    HANDLE hWinMine = CreateWinMineProcess();
+    HMODULE hAttackerModule = NULL;
     if (!hWinMine) {
         std::cerr << "ERROR: could not initialize winmine.exe" << std::endl;
-		system("pause");
+        system("pause");
         return 1;
     }
 
-	while (true) {
-		std::cout << "Welcome to the minesweeper trainer!" << std::endl
-			      << "0. Get the layout of the tiles" << std::endl
-				  << "1. Make the mines visible in game" << std::endl
-				  << "2. Freeze the timer" << std::endl
-				  << "3. Disable mines" << std::endl
-				  << "4. Auto win?" << std::endl
-				  << "5. Quit" << std::endl
-			      << "Please select an option ===> " << std::flush;
-		std::cin >> nOpt;
+    while (true) {
+        std::cout << "Welcome to the minesweeper trainer!" << std::endl
+                  << "0. Get the layout of the tiles" << std::endl
+                  << "1. Make the mines visible" << std::endl
+                  << "2. Freeze the timer" << std::endl
+                  << "3. Disable mines" << std::endl
+                  << "4. Auto win?" << std::endl
+                  << "5. Quit" << std::endl
+                  << "Please select an option ===> " << std::flush;
+        std::cin >> nOpt;
 
         if (nOpt >= 0 && nOpt < 5) {
             // all the work is done in each's respective DllMain
             hAttackerModule = DllInject(jtOpt[nOpt], hWinMine);
             system("pause");
             DllFree(hAttackerModule, hWinMine);
+            hAttackerModule = NULL;
         } else if (nOpt == 5) {
             std::cout << "Bye! <3 ~~" << std::endl;
             break;
         }
-	}
+    }
+
     // bad voodoo...
     TerminateProcess(hWinMine, 0);
     CloseHandle(hWinMine);
@@ -66,6 +68,6 @@ CreateWinMineProcess()
     // create winmine.exe
     CreateProcess("winmine.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL,
                   &si, &pi);
-	CloseHandle(pi.hThread);
+    CloseHandle(pi.hThread);
     return pi.hProcess;
 }
