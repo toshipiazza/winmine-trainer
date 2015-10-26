c0e033b698870053ea483cf6cc91d24c4f6dfbd3
Toshi R Piazza <toshi.piazza@gmail.com>
2015-10-26 01:12:41 -0400

    Prototype for get layout

--- common_globals.h
+++ common_globals.h
@@ -1,17 +1,22 @@
 #ifndef COMMON_GLOBALS_H
 #define COMMON_GLOBALS_H
 
+// case 0, get tile layout
+
 /* Type: const char tiles_on_screen[0x20][0x10]
  * Format: first/last chars on row = 0x10 (filler)
  */
-char **tiles_on_screen = reinterpret_cast<char **>(0x01005360);
-enum class visible_tiles_t {
-    DEPRESSED = 0x40, NORMAL = 0x0F, BOMB = 0xCC,
-    ONE = 0x41, TWO = 0x42, THREE = 0x42, FOUR = 0x44
+unsigned char *tiles_on_screen = reinterpret_cast<unsigned char *>(0x01005360);
+enum visible_tiles_t {
+    DEPRESSED = 0x40, NORMAL = 0x0F,
+    BOMB = 138, HIDDEN_BOMB = 143, RED_BOMB = 204,
+    ONE = 0x41, TWO = 0x42, THREE = 0x43, FOUR = 0x44
 };
+unsigned int *xBoxMac = reinterpret_cast<unsigned int *>(0x01005334);
+unsigned int *yBoxMac = reinterpret_cast<unsigned int *>(0x01005338);
 
 // case 1, make mines visible
-void (__stdcall *ShowBombs)(int) = 
+void (__stdcall *ShowBombs)(int) =
     reinterpret_cast<void(__stdcall *)(int)>(0x01002F80);
 
 // case 3, freeze timer
@@ -19,7 +24,7 @@ void(__stdcall *PauseGame)(void) =
     reinterpret_cast<void(__stdcall *)(void)>(0x0100341C);
 
 // case 4, auto win
-int *cBombLeft = 
+int *cBombLeft =
     reinterpret_cast<int *>(0x01005194);
 void (__stdcall *GameOver)(int) = // takes 1 or 0
     reinterpret_cast<void(__stdcall *)(int)>(0x0100347C);
--- get_layout.cpp
+++ get_layout.cpp
@@ -1,7 +1,7 @@
 #include <Windows.h>
-
-#define UNUSED(c) (c) = (c)
+#include "common_globals.h"
 #include <sstream>
+#include <iomanip>
 
 void
 extract_tiles_layout(std::stringstream &);
@@ -9,6 +9,7 @@ extract_tiles_layout(std::stringstream &);
 BOOL WINAPI
 DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
+    #define UNUSED(c) (c) = (c)
     UNUSED(hinstDLL);
     UNUSED(lpvReserved);
     std::stringstream ss;
@@ -30,5 +31,26 @@ DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 void
 extract_tiles_layout(std::stringstream &ss)
 {
-    ss << "Method not implemented" << std::endl;
+    ss << "LEGEND:" << std::endl
+       << "empty tile = 0" << std::endl
+       << "unpressed tile = /" << std::endl
+       << "exploded bomb = x" << std::endl
+       << "red bomb (set off) = X" << std::endl
+       << "hidden bomb = *" << std::endl << std::endl;
+    for (unsigned int i = 0; i < *xBoxMac; ++i) {
+        for (unsigned int j = 1; j <= *yBoxMac; ++j) {
+            switch (*(tiles_on_screen + j + 0x20 * i)) {
+            case DEPRESSED: ss << "0"; break;
+            case ONE: ss << "1"; break;
+            case TWO: ss << "2"; break;
+            case THREE: ss << "3"; break;
+            case FOUR: ss << "4"; break;
+            case NORMAL: ss << "/"; break;
+            case BOMB: ss << "x"; break;
+            case RED_BOMB: ss << "X"; break;
+            case HIDDEN_BOMB: ss << "*"; break;
+            }
+        }
+        ss << std::endl;
+    }
 }
